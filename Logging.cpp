b600d6127d29abbd109241506196b8f8a0c64e59
Rozhnov Alexandr <nox73@ya.ru>
2014-08-07 21:56:49 +0400

    Add flash helper support.

--- Logging.cpp
+++ Logging.cpp
@@ -1,119 +1,109 @@
 #include "Logging.h"
 
 void Logging::Init(int level, long baud){
-    _level = constrain(level,LOG_LEVEL_NOOUTPUT,LOG_LEVEL_VERBOSE);
-    _baud = baud;
-    Serial.begin(_baud);
+  _level = constrain(level,LOG_LEVEL_NOOUTPUT,LOG_LEVEL_VERBOSE);
+  _baud = baud;
+  Serial.begin(_baud);
 }
 
-void Logging::Error(char* msg, ...){
-    if (LOG_LEVEL_ERRORS <= _level) {   
-		print ("ERROR: ",0);
-        va_list args;
-        va_start(args, msg);
-        print(msg,args);
+void Logging::print(const __FlashStringHelper *format, va_list args) {
+  PGM_P p = reinterpret_cast<PGM_P>(format);
+  char c;
+  for(;c != 0; c = pgm_read_byte(p++)){
+    if (c == '%') {
+      c = pgm_read_byte(p++);
+      printFormat(c, args);
+    } else {
+      Serial.print(c);
     }
+  }
+
 }
 
-
-void Logging::Info(char* msg, ...){
-    if (LOG_LEVEL_INFOS <= _level) {
-        va_list args;
-        va_start(args, msg);
-        print(msg,args);
+void Logging::print(const char *format, va_list args) {
+  for (; *format != 0; ++format) {
+    if (*format == '%') {
+      ++format;
+      printFormat(*format, args);
+    } else {
+      Serial.print(*format);
     }
+  }
 }
 
-void Logging::Debug(char* msg, ...){
-    if (LOG_LEVEL_DEBUG <= _level) {
-        va_list args;
-        va_start(args, msg);
-        print(msg,args);
+void Logging::printFormat(const char format, va_list args) {
+  if (format == '\0') return;
+
+  if (format == '%') {
+    Serial.print(format);
+    return;
+  }
+
+  if( format == 's' ) {
+    register char *s = (char *)va_arg( args, int );
+    Serial.print(s);
+    return;
+  }
+
+  if( format == 'd' || format == 'i') {
+    Serial.print(va_arg( args, int ),DEC);
+    return;
+  }
+
+  if( format == 'x' ) {
+    Serial.print(va_arg( args, int ),HEX);
+    return;
+  }
+
+  if( format == 'X' ) {
+    Serial.print("0x");
+    Serial.print(va_arg( args, int ),HEX);
+    return;
+  }
+
+  if( format == 'b' ) {
+    Serial.print(va_arg( args, int ),BIN);
+    return;
+  }
+
+  if( format == 'B' ) {
+    Serial.print("0b");
+    Serial.print(va_arg( args, int ),BIN);
+    return;
+  }
+
+  if( format == 'l' ) {
+    Serial.print(va_arg( args, long ),DEC);
+    return;
+  }
+
+  if( format == 'c' ) {
+    Serial.print(va_arg( args, int ));
+    return;
+  }
+
+  if( format == 't' ) {
+    if (va_arg( args, int ) == 1) {
+      Serial.print("T");
     }
+    else {
+      Serial.print("F");
+    }
+    return;
+  }
+
+  if( format == 'T' ) {
+    if (va_arg( args, int ) == 1) {
+      Serial.print(F("true"));
+    }
+    else {
+      Serial.print(F("false"));
+    }
+    return;
+  }
+
 }
 
-
-void Logging::Verbose(char* msg, ...){
-    if (LOG_LEVEL_VERBOSE <= _level) {
-        va_list args;
-        va_start(args, msg);
-        print(msg,args);
-    }
-}
-
-
-
- void Logging::print(const char *format, va_list args) {
-    //
-    // loop through format string
-    for (; *format != 0; ++format) {
-        if (*format == '%') {
-            ++format;
-            if (*format == '\0') break;
-            if (*format == '%') {
-                Serial.print(*format);
-                continue;
-            }
-            if( *format == 's' ) {
-				register char *s = (char *)va_arg( args, int );
-				Serial.print(s);
-				continue;
-			}
-            if( *format == 'd' || *format == 'i') {
-				Serial.print(va_arg( args, int ),DEC);
-				continue;
-			}
-            if( *format == 'x' ) {
-				Serial.print(va_arg( args, int ),HEX);
-				continue;
-			}
-            if( *format == 'X' ) {
-				Serial.print("0x");
-				Serial.print(va_arg( args, int ),HEX);
-				continue;
-			}
-            if( *format == 'b' ) {
-				Serial.print(va_arg( args, int ),BIN);
-				continue;
-			}
-            if( *format == 'B' ) {
-				Serial.print("0b");
-				Serial.print(va_arg( args, int ),BIN);
-				continue;
-			}
-            if( *format == 'l' ) {
-				Serial.print(va_arg( args, long ),DEC);
-				continue;
-			}
-
-            if( *format == 'c' ) {
-				Serial.print(va_arg( args, int ));
-				continue;
-			}
-            if( *format == 't' ) {
-				if (va_arg( args, int ) == 1) {
-					Serial.print("T");
-				}
-				else {
-					Serial.print("F");				
-				}
-				continue;
-			}
-            if( *format == 'T' ) {
-				if (va_arg( args, int ) == 1) {
-					Serial.print("true");
-				}
-				else {
-					Serial.print("false");				
-				}
-				continue;
-			}
-
-        }
-        Serial.print(*format);
-    }
- }
- 
  Logging Log = Logging();
 
  
